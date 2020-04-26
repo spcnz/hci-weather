398c4b010224872e9bdb78dc2da1860e158b7c04
specnazm <laketic.milena98@gmail.com>
2020-04-26 17:41:23 +0200

    Changed legend on chart

--- hci-weather/src/components/Graph.js
+++ hci-weather/src/components/Graph.js
@@ -3,12 +3,12 @@ import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Label } 
 
 
 function CustomTooltip({ payload, label, active }) {
+  console.log(payload)
   if (active) {
     return (
       <div className="custom-tooltip">
-        <p className="label">{`Time ${label} : ${property} ${payload[0].value}`}</p>
+        <p className="label">{`Time ${label} : ${labels[property]} ${payload[0].value}`}</p>
         <p className="intro">{`Date: ${payload[0].payload.date}`}</p>
-        <p className="desc">{`City: ${payload[0].payload.city}`}</p>
       </div>
     );
   }
@@ -19,9 +19,7 @@ function CustomTooltip({ payload, label, active }) {
 
 const property = 'feels_like'
 
-
 function createData ({dt_txt, main}, city, index) {
-  console.log(main)
   let  object = {}
   const dateTime = dt_txt.split(' ')
   const date = dateTime[0]
@@ -32,7 +30,11 @@ function createData ({dt_txt, main}, city, index) {
   return {time, date, ...object}
 }
 
-
+function getName({city}) {
+  let cityName = city.name + ', ' + city.country
+  cityName = cityName.replace(', undefined', '')
+  return cityName
+}
 
 function getRandomColor() {
   var letters = '0123456789ABCDEF';
@@ -43,7 +45,7 @@ function getRandomColor() {
   return color;
 }
 
-const colors = ['#ffff00','#7e00ff', '#00ff40', '#ffbf00' ,'#000000','#ff0000','#283593']
+const colors = ['#283593', '#ff0000', '#000000', '#ffbf00', '#00ff40', '#7e00ff', '#ffff00']
 const labels = {pressure: 'Pressure hpa', temp: 'Temperature °C', feels_like: 'Feels like °C', humidity: 'Humidity %'}
 
 export default  function renderLineChart(props) {
@@ -54,10 +56,11 @@ export default  function renderLineChart(props) {
   let dataMax = Number.MIN_VALUE
 
   data.forEach(((forecast, index) => {
-    const city = forecast.city.name + ', ' + forecast.city.country
+    
     let countDays = -1
     let prevDate = ''
     console.log("FORECAST ", forecast)
+    const city = getName(forecast)
 
     forecast.list.forEach(item => {
       const elData = createData(item, city, index)
@@ -80,45 +83,43 @@ export default  function renderLineChart(props) {
     })
   }))
 
-  console.log(property)
-
   return (
-  <LineChart width={600} height={500} data={chartData} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-  { data.map((forecast, index) => {
-      const color = index >= colors.length? getRandomColor() : colors[colors.length - index - 1]
-      return (
-        <Line type="monotone" dataKey={property + index} stroke={color} key={index}/>
-      )})
-  }
-  <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-  <XAxis dataKey="time" dy={5}>
-  <Label
-    value='Measurement time'
-    position="insideBottomRight"
-    fontSize={18}
-    fill='#283593'
-    align='right'
-    offset={-15}
-      />
-  </XAxis>
-  <Legend align="center" />
-  <YAxis 
-    dx={-5} 
-    type="number"
-    domain={[dataMin - 10, dataMax + 10]}
-    dataKey={property}
-      >
-  <Label
-    value={labels[property]}
-    position="insideLeft"
-    fontSize={18}
-    fill='#283593'
-    align='center'
-    angle={270}
-    offset={7}
-      />
-  </YAxis>
-  <Tooltip content={<CustomTooltip />} wrapperStyle={{ width: 100, backgroundColor: '#ccc' }} />
-</LineChart>
+    <LineChart width={600} height={500} data={chartData} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+    { data.map((forecast, index) => {
+        const color = index >= colors.length? getRandomColor() : colors[index]
+        return (
+          <Line name={getName(forecast)} type="monotone" dataKey={property + index} stroke={color} key={index}/>
+        )})
+    }
+    <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+    <XAxis dataKey="time" dy={5}>
+    <Label
+      value='Measurement time'
+      position="insideBottomRight"
+      fontSize={18}
+      fill='#283593'
+      align='right'
+      offset={-15}
+        />
+    </XAxis>
+    <Legend align="center" />
+    <YAxis 
+      dx={-5} 
+      type="number"
+      domain={[dataMin - 10, dataMax + 10]}
+      dataKey={property}
+        >
+    <Label
+      value={labels[property]}
+      position="insideLeft"
+      fontSize={18}
+      fill='#283593'
+      align='center'
+      angle={270}
+      offset={7}
+        />
+    </YAxis>
+    <Tooltip content={<CustomTooltip />} wrapperStyle={{ width: 100, backgroundColor: '#ccc' }} />
+  </LineChart>
   
 )}
--- hci-weather/src/components/Home.js
+++ hci-weather/src/components/Home.js
@@ -97,6 +97,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+
 export default function Home() {
 
     const classes = useStyles();
